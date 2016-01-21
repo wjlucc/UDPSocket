34b7538696f00781c415bbbacff00f4a5d6d96ed
wjlucc <wjlucc@gmail.com>
2016-01-21 19:28:27 +0800

    服务器端收到客户端的通知后，增加了响应功能。

---
+++ src/com/UDPdemo2/UDPClient.java
@@ -0,0 +1,48 @@
+package com.UDPdemo2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+
+public class UDPClient {
+
+	public static void main(String[] args) throws IOException {
+
+		// 1.定义发送的数据信息。服务器的地址，端口号，数据。
+		InetAddress address = InetAddress.getByName("localhost"); // 用于表示服务器的地址。
+		int port = 8880; // 指定端口号，和服务器端相同。
+		byte[] info = "用户名：admin；密码：1234".getBytes(); // 字符串重要的方法，可以将一个字符串转换成字节数组。
+
+		// 2.创建要发送的信息的数据报 DatagramPacket，包含要发送的数据信息。。？这里选用的是包含四个参数的构造方法？
+		DatagramPacket datagramPacket = new DatagramPacket(info, info.length, address, port);
+
+		// 3.创建DatagramSocket，用来实现数据的发送。。。？？这里构造方法不需要参数？
+		DatagramSocket datagramSocket = new DatagramSocket();
+		
+		//DatagramSocket应该只是相当于一个邮筒的作用，不需要指定向那个位置传送。
+		//而DatagramPacket是信件，上面包括了要传送的所有相关信息。包括地址，端口号，数据三部分。地址是在信件上面的。
+		
+		// 4.发送数据
+		datagramSocket.send(datagramPacket);
+		
+		//添加接收服务器的消息
+		
+		//1.创建用于接收的邮筒 DatagramSocket，已经创建好了
+		//2.创建等待接收的报文
+		byte[] data = new byte[1024];
+		DatagramPacket datagramPacket2 = new DatagramPacket(data, data.length);
+		
+		//3.接收消息
+		datagramSocket.receive(datagramPacket2);
+		
+		//4.处理接收到的消息
+		String data2 = new String(data,0,data.length);
+		System.out.println(data2);
+		
+		
+		
+
+	}
+}
---
+++ src/com/UDPdemo2/UDPServer.java
@@ -0,0 +1,45 @@
+package com.UDPdemo2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+	public static void main(String[] args) throws IOException {
+		
+		//1.创建一个DatagramSocket，指定端口号，是通信链的端头。
+		DatagramSocket datagramSocket = new DatagramSocket(8880);
+		
+		//2.创建一个DatagramPacket 数据报，用于接收客户端发送的数据。
+		byte[] info = new byte[1024];	//数据报中的数据是保存在字节数组中的。
+		DatagramPacket datagramPacket = new DatagramPacket(info, info.length);//将收到的数据存在info数组中。
+		
+		System.out.println("-----服务器端已经启动-----");
+		//3.等待接收客户端发送的数据报文，直到收到报文。
+		datagramSocket.receive(datagramPacket);	//此方法在接收到数据之前会一直处于阻塞状态。
+		
+		//4.读取接收到的消息
+		String data = new String(info,0,info.length);
+		System.out.println("收到消息："+data);
+//		System.out.println("收到消息："+Arrays.toString(info));
+		
+		
+		//添加服务器的响应信息
+		
+		//1.定义要发送 的信息，地址，端口号，信息。
+		InetAddress address = datagramPacket.getAddress();
+		int port = datagramPacket.getPort();
+		byte[] data2 = "欢迎登陆！".getBytes();
+		
+		//2.创建要发送的数据报文
+		DatagramPacket datagramPacket2 = new DatagramPacket(data2, data2.length, address, port);
+		
+		//3.发送报文的邮筒DatagramSocket已经被建好了
+		
+		//4.发送信息
+		
+		datagramSocket.send(datagramPacket2);
+		
+	}
+}
